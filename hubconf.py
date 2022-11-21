eacce6977725eb5ae712a0507ca2b4f5c6836109
Himanshu DOLEKAR <43377009+cs21m007iittp@users.noreply.github.com>
2022-11-21 12:30:52 +0530

    Update hubconf.py

--- hubconf.py
+++ hubconf.py
@@ -1,196 +1,282 @@
-
-# practice torchhub on other ds..
-# using cifar10..
-
 import torch
 from torch import nn
-from torch.utils.data import DataLoader
-from torchvision import datasets
-from torchvision.transforms import ToTensor
-import torchvision.transforms as transforms
-
-device="cuda" if torch.cuda.is_available() else "cpu"
-print(f"Using {device} device")
-
-classes = [
-    "airplane",
-    "automobile",
-    "bird",
-    "cat",
-    "deer",
-    "dog",
-    "frog",
-    "horse",
-    "ship",
-    "truck"
-]
+import torch.optim as optim
+from sklearn.datasets import make_blobs
+from sklearn.datasets import make_circles
+from sklearn.datasets import load_digits
+from sklearn.cluster import KMeans
+from sklearn import metrics
+from sklearn.metrics import homogeneity_score,completeness_score,v_measure_score
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report,recall_score,roc_auc_score,precision_score,f1_score
+from sklearn.model_selection import GridSearchCV
 
 
-transform_train = transforms.Compose([
-    transforms.RandomCrop(32, padding=4),
-    transforms.RandomHorizontalFlip(),
-    transforms.ToTensor(),
-    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
-])
-
-transform_test = transforms.Compose([
-    transforms.RandomCrop(32, padding=4),
-    transforms.RandomHorizontalFlip(),
-    transforms.ToTensor(),
-    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
-])
 
 
-# Define model
-class NeuralNetwork(nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.linear_relu_stack = nn.Sequential(
-           
-        
 
-            nn.Conv2d(3, 32, kernel_size=3, padding=1),
-            nn.ReLU(),
-            nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1),
-            nn.ReLU(),
-            nn.MaxPool2d(2, 2), # output: 64 x 16 x 16
-            nn.BatchNorm2d(64),
+# You can import whatever standard packages are required
 
-            nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1),
-            nn.ReLU(),
-            nn.Conv2d(128, 128, kernel_size=3, stride=1, padding=1),
-            nn.ReLU(),
-            nn.MaxPool2d(2, 2), # output: 128 x 8 x 8
-            nn.BatchNorm2d(128),
+# full sklearn, full pytorch, pandas, matplotlib, numpy are all available
+# Ideally you do not need to pip install any other packages!
+# Avoid pip install requirement on the evaluation program side, if you use above packages and sub-packages of them, then that is fine!
 
-            nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1),
-            nn.ReLU(),
-            nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1),
-            nn.ReLU(),
-            nn.MaxPool2d(2, 2), # output: 256 x 4 x 4
-            nn.BatchNorm2d(256),
+###### PART 1 ######
 
-            nn.Flatten(), 
-            nn.Linear(256*4*4, 1024),
-            nn.ReLU(),
-            nn.Linear(1024, 512),
-            nn.ReLU(),
-            nn.Linear(512, 10))
+def get_data_blobs(n_points=100):
+  
+  # write your code here
+  # Refer to sklearn data sets
+  X, y = make_blobs(n_samples=n_points)
+  # write your code ...
+  return X,y
 
-    def forward(self, x):
-        logits = self.linear_relu_stack(x)
-        return logits
+def get_data_circles(n_points=100):
+  
+  # write your code here
+  # Refer to sklearn data sets
+  X, y = make_circles(n_samples=n_points)
+  # write your code ...
+  return X,y
 
-def get_lossfn_and_optimizer(mymodel):
-    loss_fn = nn.CrossEntropyLoss()
-    optimizer = torch.optim.SGD(mymodel.parameters(), lr=1e-3)
-    return loss_fn, optimizer
+def get_data_mnist():
+  
+  # write your code here
+  # Refer to sklearn data sets
+  
+  digits= load_digits()
+  X = digits.data
+  y = digits.target
+  
+  # write your code ...
+  return X,y
 
-def load_data():
+def build_kmeans(X=None,k=10):
+  
+  # k is a variable, calling function can give a different number
+  # Refer to sklearn KMeans method
+  km = KMeans(n_clusters=k).fit(X)
+  
+  return km
 
-    # Download training data from open datasets.
-    # using cifar10 dataset...
-    training_data = datasets.CIFAR10(
-        root="data",
-        train=True,
-        download=True,
-        transform=transform_train,
-    )
+def assign_kmeans(km,X):
+  
+  # For each of the points in X, assign one of the means
+  # refer to predict() function of the KMeans in sklearn
+  # write your code ...
+  ypred = km.predict(X)
+  return ypred
 
-    # Download test data from open datasets.
-    test_data = datasets.CIFAR10(
-        root="data",
-        train=False,
-        download=True,
-        transform=transform_test,
-    )
+def compare_clusterings(ypred_1=None,ypred_2=None):
+  
+  # refer to sklearn documentation for homogeneity, completeness and vscore
+  h=homogeneity_score(ypred_1,ypred_2)
+  c=completeness_score(ypred_1,ypred_2)
+  v=v_measure_score(ypred_1,ypred_2)
+  
+  print(h)
+  print(c)
+  print(v)
+  
+  return h,c,v
+
+###### PART 2 ######
+
+def build_lr_model(X, y):
+  
+  lr_model = LogisticRegression().fit(X, y)
+  # write your code...
+  # Build logistic regression, refer to sklearn
+  return lr_model
+
+def build_rf_model(X, y):
+  
+  
+  # write your code...
+  # Build Random Forest classifier, refer to sklearn
+  rf_model=RandomForestClassifier()
+  rf_model.fit(X, y)
+  
+  return rf_model
+
+def get_metrics(model1,X,y):
+  
+  y_test=y
+  y_pred_test = model1.predict(X)
+  
+  
+  
+  
+  
+  # View accuracy score
+  acc=accuracy_score(y_test, y_pred_test)
+  # print(acc)
+  rec=recall_score(y_test,y_pred_test,average='macro')
+  #print(rec)
+  prec=precision_score(y_test,y_pred_test,average='macro')
+  #print(prec)
+  f1=f1_score(y_test,y_pred_test,average='macro')
+  
+  fpr, tpr, thresholds = metrics.roc_curve(y, ypred_test, pos_label=2)
+  auc = metrics.auc(fpr, tpr)
+  
+  #auc=roc_auc_score(y_test,y_pred_test,multi_class='ovr')
+  #print(auc)
+  # write your code here...
+  
+  return acc, prec, rec, f1, auc
+
+
+# part 2b ...
+
+
+def get_paramgrid_lr():
+  # you need to return parameter grid dictionary for use in grid search cv
+  # penalty: l1 or l2
+  
+  lr_param_grid = {'penalty' : ['l1','l2']}
+
+  
+  # refer to sklearn documentation on grid search and logistic regression
+  # write your code here...
+  return lr_param_grid
+
+def get_paramgrid_rf():
+  # you need to return parameter grid dictionary for use in grid search cv
+  # n_estimators: 1, 10, 100
+  # criterion: gini, entropy
+  # maximum depth: 1, 10, None  
+  rf_param_grid = { 'n_estimators' : [1,10,100],'criterion' :["gini", "entropy"], 'max_depth' : [1,10,None]  }
+  # refer to sklearn documentation on grid search and random forest classifier
+  # write your code here...
+  return rf_param_grid
+
+def perform_gridsearch_cv_multimetric(model=None, param_grid=None, cv=5, X=None, y=None, metrics=['accuracy','roc_auc']):
+  
+  # you need to invoke sklearn grid search cv function
+  # refer to sklearn documentation
+  # the cv parameter can change, ie number of folds  
+  
+  # metrics = [] the evaluation program can change what metrics to choose
+  
+  grid_search_cv = None
+  # create a grid search cv object
+  # fit the object on X and y input above
+  # write your code here...
+  
+  # metric of choice will be asked here, refer to the-scoring-parameter-defining-model-evaluation-rules of sklearn documentation
+  
+  # refer to cv_results_ dictonary
+  # return top 1 score for each of the metrics given, in the order given in metrics=... list
+  
+  top1_scores = []
+  
+  
+  if X.ndim > 2:
+      n_samples = len(X)
+      X= X.reshape((n_samples, -1))
+      
+  for score in metrics:
+      grid_search_cv = GridSearchCV(model,param_grid,scoring = score,cv=cv)
+      grid_search_cv.fit(X,y)
+      top1_scores.append(grid_search_cv.best_estimator_.get_params())
+  
+  return top1_scores
+
+###### PART 3 ######
+
+# class MyNN(nn.Module):
+#   def __init__(self,inp_dim=64,hid_dim=13,num_classes=10):
+#     super(MyNN,self).__init__()
     
-    return training_data, test_data
-
-def create_dataloaders(training_data, test_data, batch_size=128):
-
-    # Create data loaders.
-    train_dataloader = DataLoader(training_data, batch_size=batch_size,shuffle=True, num_workers=2)
-    test_dataloader = DataLoader(test_data, batch_size=batch_size,shuffle=True, num_workers=2)
-
-    for X, y in test_dataloader:
-        print(f"Shape of X [N, C, H, W]: {X.shape}")
-        print(f"Shape of y: {y.shape} {y.dtype}")
-        break
-    for X, y in train_dataloader:
-        print(f"Shape of X [N, C, H, W]: {X.shape}")
-        print(f"Shape of y: {y.shape} {y.dtype}")
-        break
-
-        
-    return train_dataloader, test_dataloader
-
-def get_model():
+#     self.flat = nn.Flatten()
+#     self.fc_encoder = nn.Linear(inp_dim,hid_dim).to(device) # write your code inp_dim to hid_dim mapper
+#     self.fc_decoder = nn.Linear(hid_dim,inp_dim).to(device) # write your code hid_dim to inp_dim mapper
+#     self.fc_classifier = nn.Linear(hid_dim,num_classes).to(device) # write your code to map hid_dim to num_classes
     
-    model = NeuralNetwork().to(device)
-
-    return model
-
-def _train(dataloader, model, loss_fn, optimizer):
-    size = len(dataloader.dataset)
-    model.train()
-    for batch, (X, y) in enumerate(dataloader):
-        X, y = X.to(device), y.to(device)
-
-        # Compute prediction error
-        pred = model(X)
-        loss = loss_fn(pred, y)
-
-        # Backpropagation
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-
-        if batch % 100 == 0:
-            loss, current = loss.item(), batch * len(X)
-            print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
-
-def _test(dataloader, model, loss_fn):
-    size = len(dataloader.dataset)
-    num_batches = len(dataloader)
-    model.eval()
-    test_loss, correct = 0, 0
-    with torch.no_grad():
-        for X, y in dataloader:
-            X, y = X.to(device), y.to(device)
-            pred = model(X)
-            test_loss += loss_fn(pred, y).item()
-            correct += (pred.argmax(1) == y).type(torch.float).sum().item()
-    test_loss /= num_batches
-    correct /= size
-    print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
-
-def train(train_dataloader, test_dataloader, model1, loss_fn1, optimizer1, epochs=5):
-    for t in range(epochs):
-        print(f"Epoch {t+1}\n-------------------------------")
-        _train(train_dataloader, model1, loss_fn1, optimizer1)
-        _test(test_dataloader, model1, loss_fn1)
-    print("Done!")
-    return model1
-
-def save_model(model1,mypath="model.pth"):
-    torch.save(model1.state_dict(), "model.pth")
-    print("Saved PyTorch Model State to model.pth")
-
-def load_model(mypath="model.pth"):
-    model = NeuralNetwork()
-    model.load_state_dict(torch.load("model.pth"))
-    return model
-
-def sample_test(model1,test_loader):
-    model1.eval()
-   # x, y = test_data.__getitem__(0)
-
+#     self.relu = nn.ReLU() #write your code - relu object
+#     self.softmax = nn.Softmax() #write your code - softmax object
     
-    with torch.no_grad():
-        for x,y in test_loader:
-            pred = model1(x.cuda())
-            predicted, actual = classes[pred.argmax(1)], y
-            print(f'Predicted: "{predicted}", Actual: "{actual}"')
-            break
+#  def forward(self,x):
+#     x = self.flat(x) # write your code - flatten x
+#     x_enc = self.fc_encoder(x)
+#     x_enc = self.relu(x_enc)
+    
+#     y_pred = self.fc_classifier(x_enc)
+#     y_pred = self.softmax(y_pred)
+    
+#     x_dec = self.fc_decoder(x_enc)
+    
+#     return y_pred, x_dec
+  
+#   # This a multi component loss function - lc1 for class prediction loss and lc2 for auto-encoding loss
+# def loss_fn(self,x,yground,y_pred,xencdec):
+    
+#     # class prediction loss
+#     # yground needs to be one hot encoded - write your code
+#     # write your code for cross entropy between yground and y_pred, advised to use torch.mean()
+#     classes = set()
+#     for i in yground:
+#       classes.add(i)
+#     num_classes = len(classes)
+#     tmp = Fun.one_hot(yground, num_classes= num_classes).to(device)
+    
+#     y_pred , tmp = y_pred.to(device) , tmp.to(device)
+#     v = -(tmp * torch.log(y_pred + 0.0001))
+#     lc1 = torch.mean(v)
 
+
+#     # auto encoding loss
+#     lc2 = torch.mean((x - xencdec)**2)
+    
+#     lval = lc1 + lc2
+    
+#     return 
+    
+# def get_mynn(inp_dim=64,hid_dim=13,num_classes=10):
+#   mynn = MyNN(inp_dim,hid_dim,num_classes)
+#   mynn.double()
+#   return mynn
+
+# def get_mnist_tensor():
+#   # download sklearn mnist
+#   # convert to tensor
+#   X_np, y_np = get_data_mnist()
+
+#   X = torch.tensor(X_np)
+#   y = torch.tensor(y_np)
+#   # write your code
+#   return X,y
+
+# def get_loss_on_single_point(mynn=None,x0,y0):
+#   y_pred, xencdec = mynn(x0)
+#   lossval = mynn.loss_fn(x0,y0,y_pred,xencdec)
+#   # the lossval should have grad_fn attribute set
+#   return lossval
+
+# def train_combined_encdec_predictor(mynn=None,X,y, epochs=11):
+#   # X, y are provided as tensor
+#   # perform training on the entire data set (no batches etc.)
+#   # for each epoch, update weights
+  
+#   optimizer = optim.SGD(mynn.parameters(), lr=0.01)
+  
+#   for i in range(epochs):
+#     optimizer.zero_grad()
+#     ypred, Xencdec = mynn(X)
+#     lval = mynn.loss_fn(X,y,ypred,Xencdec)
+#     lval.backward()
+#     optimzer.step()
+    
+#   return mynn
+    
+
+
+
+
+  
+  
+  
